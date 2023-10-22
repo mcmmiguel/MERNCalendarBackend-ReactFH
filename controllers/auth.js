799fb96ca19b0ac8c75d8453c4dc1f4eabf5eddb
Miguel Cobian <miguelcobian99@gmail.com>
2023-10-21 19:17:31 -0600

    Refactor: Add middleware to handle errors

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,8 @@
 const express = require('express');
-const { validationResult } = require('express-validator');
 
 const crearUsuario = (req, res = express.response) => {
 
     const { name, email, password } = req.body;
-
-    // Manejo de errores
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped(),
-        });
-    }
 
     res.status(201).json({
         ok: true,
@@ -27,16 +16,6 @@ const crearUsuario = (req, res = express.response) => {
 const loginUsuario = (req, res = express.response) => {
 
     const { name, email, password } = req.body;
-
-    // Manejo de errores
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped(),
-        });
-    }
 
     res.status(201).json({
         ok: true,
--- routes/auth.js
+++ routes/auth.js
@@ -6,12 +6,14 @@ const { Router } = require('express');
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
 const router = Router();
 const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 router.post('/new',
     [
         check('name', 'El nombre es obligatorio').not().isEmpty(),
         check('email', 'El email es obligatorio').isEmail(),
         check('password', 'El password debe tener al menos 6 caracteres').isLength({ min: 6 }),
+        validarCampos,
     ],
     crearUsuario);
 
@@ -19,6 +21,7 @@ router.post('/',
     [
         check('email', 'El email es obligatorio').isEmail(),
         check('password', 'El password debe tener al menos 6 caracteres').isLength({ min: 6 }),
+        validarCampos,
     ],
     loginUsuario);
 
