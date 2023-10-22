e4ed8ac09ce9881a14f79e49c7600892939e0bb8
Miguel Cobian <miguelcobian99@gmail.com>
2023-10-22 12:28:16 -0600

    Feat. Create our first user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,28 @@
 const express = require('express');
+const Usuario = require('../models/Usuario');
 
-const crearUsuario = (req, res = express.response) => {
+const crearUsuario = async (req, res = express.response) => {
 
-    const { name, email, password } = req.body;
+    // const { name, email, password } = req.body;
 
-    res.status(201).json({
-        ok: true,
-        msg: 'registro',
-        name,
-        email,
-        password,
-    });
+    try {
+
+        const usuario = new Usuario(req.body);
+
+        await usuario.save();
+
+        res.status(201).json({
+            ok: true,
+            msg: 'registro',
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador',
+        })
+    }
+
 };
 
 const loginUsuario = (req, res = express.response) => {
