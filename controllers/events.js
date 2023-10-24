a94999fc5c1cb5667e78aa70e0a1e8d587271d8f
Miguel Cobian <miguelcobian99@gmail.com>
2023-10-24 10:30:33 -0600

    Feat: Get the event list

--- controllers/events.js
+++ controllers/events.js
@@ -1,11 +1,14 @@
 const { response } = require('express');
 const Evento = require('../models/Evento');
 
-const getEventos = (req, res = response, next) => {
+const getEventos = async (req, res = response) => {
+
+    const eventos = await Evento.find()
+        .populate('user', 'name');
 
     res.json({
         ok: true,
-        msg: 'getEventos'
+        msg: eventos,
     })
 }
 const crearEvento = async (req, res = response, next) => {
