146fc150dac8f2683b8df41f96cacf7eacd98fe2
Miguel Cobian <miguelcobian99@gmail.com>
2023-10-24 10:53:59 -0600

    Feat: Update events

--- controllers/events.js
+++ controllers/events.js
@@ -33,12 +33,50 @@ const crearEvento = async (req, res = response, next) => {
         });
     }
 }
-const actualizarEvento = (req, res = response, next) => {
+const actualizarEvento = async (req, res = response, next) => {
 
-    res.json({
-        ok: true,
-        msg: 'actualizarEvento'
-    })
+    const eventoID = req.params.id;
+    const uid = req.uid;
+
+    try {
+
+        const evento = await Evento.findById(eventoID);
+        if (!evento) {
+            res.status(404).json({
+                ok: false,
+                msg: 'No existe evento con ese ID'
+            });
+        }
+
+        if (evento.user.toString() !== uid) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene el privilegio para editar este evento',
+            });
+        }
+
+        const nuevoEvento = {
+            ...req.body,
+            user: uid,
+        };
+
+        const eventoActualizado = await Evento.findByIdAndUpdate(eventoID, nuevoEvento, { new: true });
+
+        res.json({
+            ok: true,
+            evento: eventoActualizado,
+        })
+
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador',
+        })
+    }
+
 }
 const eliminarEvento = (req, res = response, next) => {
 
